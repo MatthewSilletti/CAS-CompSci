2bbdf4f406f9f870babd1f5fc178eb45c26614e6
Matthew <matthew.s21@k12.xrds.org>
2019-10-21 10:12:36 -0700

    Work

---
+++ Swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str: String = "Hello, playground"
+str = "Goodbye, retard"
+
+var speed: Int
+speed = 4
+
+var experiment = "Lets Try This"
+experiment = "5"
+
+var total = 7 + Int(experiment)!
+
+Int(str)
