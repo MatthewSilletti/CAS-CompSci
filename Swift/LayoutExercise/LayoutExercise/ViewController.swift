c5019cf927ebe20104acdf3bbfbda50055a384a8
Matthew <matthew.s21@k12.xrds.org>
2020-02-02 22:27:32 -0800

    layout exercise

---
+++ Swift/LayoutExercise/LayoutExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutExercise
+//
+//  Created by Matthew Silletti on 1/29/20.
+//  Copyright © 2020 Matthew Silletti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
